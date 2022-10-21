f7b70a1c55ecf5c67253e9d5dc880cf07822655a
Mohammad Anwar <mohammad.anwar2939@gmail.com>
2022-10-21 10:40:52 -0400

    Add files via upload

--- assignmentElevenAnwar/src/assignmentelevenanwar/AssignmentElevenAnwar.java
+++ assignmentElevenAnwar/src/assignmentelevenanwar/AssignmentElevenAnwar.java
@@ -4,18 +4,65 @@
  * and open the template in the editor.
  */
 package assignmentelevenanwar;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+// @author Mohammad Anwar
 
-/**
- *
- * @author s201076699
- */
+
 public class AssignmentElevenAnwar {
 
-    /**
-     * @param args the command line arguments
-     */
+    static Scanner input = new Scanner(System.in);
+    
+    static void stayAtHome(){
+        System.out.println("Will you wake up at 10 AM or 12PM? (10/12)");
+        //ArrayList<Object> cars = new ArrayList<Object>({"The questions":{"sol1", "nextquestionnumber"},{                                                                                                                                                                                                                                                                                                         }});
+        int decision  = input.nextInt();
+        while(true){
+            if(decision == 10){
+                
+            } else if(decision == 12){
+            
+            }
+        }
+    }
+    
+    static void leaveForMordor(){
+        
+    }
+    
+    static void leaveWithCompany(){
+        
+    }
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        HashMap<String, Object[]> questionMap = new HashMap<String, Object[]>();
+        questionMap.put("Do you fight? ", new Object[]{"Yes", 1, "No", 3});
+        System.out.println(Arrays.toString(questionMap.get("Do you fight? "))); 
+        for (String i : questionMap.keySet()) {
+            System.out.println(i);
+            Object[] answers = (questionMap.get(i));
+            System.out.println(answers[0]);
+           
+        }
+        /*
+        while(true){
+            System.out.println("Welcome to: The Hobbit");
+            String decision = input.nextLine();
+            switch(decision){
+                case "A":
+                    leaveWithCompany();
+                    break;
+                case "B":
+                    stayAtHome();
+                    break;
+                case "C":
+                    leaveForMordor();
+                    break;   
+            }
+            
+        }*/
     }
     
 }
