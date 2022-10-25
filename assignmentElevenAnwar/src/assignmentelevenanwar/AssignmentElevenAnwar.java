7a97a49f5964c91d4680cdb25113a3563b751292
Mohammad Anwar <mohammad.anwar2939@gmail.com>
2022-10-25 09:17:18 -0400

    Add files via upload

--- assignmentElevenAnwar/src/assignmentelevenanwar/AssignmentElevenAnwar.java
+++ assignmentElevenAnwar/src/assignmentelevenanwar/AssignmentElevenAnwar.java
@@ -15,54 +15,56 @@ public class AssignmentElevenAnwar {
 
     static Scanner input = new Scanner(System.in);
     
-    static void stayAtHome(){
-        System.out.println("Will you wake up at 10 AM or 12PM? (10/12)");
-        //ArrayList<Object> cars = new ArrayList<Object>({"The questions":{"sol1", "nextquestionnumber"},{                                                                                                                                                                                                                                                                                                         }});
-        int decision  = input.nextInt();
+    static void nextDecision(Integer nextPos, HashMap map){
         while(true){
-            if(decision == 10){
-                
-            } else if(decision == 12){
-            
-            }
+                if(nextPos == 0){
+                   System.out.println("You lived life happily ever after. Bye bye.");
+                   System.exit(0);
+                } else{
+                    Object[] actions = (Object[]) map.get(nextPos);
+                    System.out.print(actions[0] + ": "); //The question
+                    String decision = input.nextLine();
+                    if (actions[1].equals(decision)) {
+                        nextDecision((Integer) actions[2], map);
+                    } else if (actions[3].equals(decision)) {
+                        nextDecision((Integer) actions[4], map);
+                    }   
+                }
         }
-    }
-    
-    static void leaveForMordor(){
-        
-    }
-    
-    static void leaveWithCompany(){
-        
     }
     
     public static void main(String[] args) {
-        HashMap<String, Object[]> questionMap = new HashMap<String, Object[]>();
-        questionMap.put("Do you fight? ", new Object[]{"Yes", 1, "No", 3});
-        System.out.println(Arrays.toString(questionMap.get("Do you fight? "))); 
-        for (String i : questionMap.keySet()) {
-            System.out.println(i);
-            Object[] answers = (questionMap.get(i));
-            System.out.println(answers[0]);
-           
-        }
-        /*
+        HashMap<Integer, Object[]> questionMap = new HashMap<Integer, Object[]>(){{
+            put(1, new Object[]{"Do you fight?", "Yes", 2, "No", 3});
+            put(2, new Object[]{"Will you leave at night", "Yes", 4, "No", 5});
+            put(3, new Object[]{"Will you sleep?", "Yes", 0, "No", 0});
+            put(4, new Object[]{"The war was over, before you arrived. Would you like to go back home?", "Yes", 0, "No", 0});
+            put(5, new Object[]{"You went to sleep instead.", "Yes", 0, "No", 0});
+        }};
+        
         while(true){
             System.out.println("Welcome to: The Hobbit");
+            System.out.print("Enter B to begin your adventure: ");
             String decision = input.nextLine();
             switch(decision){
                 case "A":
-                    leaveWithCompany();
                     break;
                 case "B":
-                    stayAtHome();
+                    System.out.println("Type in Yes/No for you answers.");
+                    Object[] actions = questionMap.get(1);
+                    System.out.print(actions[0] + ": "); //The question
+                    decision  = input.nextLine();
+                    if(actions[1].equals(decision)){
+                        nextDecision((Integer) actions[2], questionMap);
+                    } else if(actions[3].equals(decision)){
+                        nextDecision((Integer) actions[4], questionMap);
+                    }
                     break;
                 case "C":
-                    leaveForMordor();
                     break;   
             }
             
-        }*/
+        }
     }
     
 }
