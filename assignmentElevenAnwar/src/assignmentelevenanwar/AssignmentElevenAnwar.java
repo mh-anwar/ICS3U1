6b136befd6753ab1f1efb4b8fcd93688195d80f9
Mohammad Anwar <mohammad.anwar2939@gmail.com>
2022-10-28 09:20:02 -0400

    Add files via upload

--- assignmentElevenAnwar/src/assignmentelevenanwar/AssignmentElevenAnwar.java
+++ assignmentElevenAnwar/src/assignmentelevenanwar/AssignmentElevenAnwar.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package assignmentelevenanwar;
 import java.util.Scanner;
 import java.util.HashMap;
@@ -28,29 +23,64 @@ public class AssignmentElevenAnwar {
                     System.out.println(actions[0] + ": "); //The question
                     System.out.print("[Your Input (Y/N/R)] ~ ");
                     String decision = input.nextLine();
-                    
-                    if (decision.equals("Y")) {
-                        nextDecision((Integer) actions[1], map);
-                    } else if (decision.equals("N")) {
-                        nextDecision((Integer) actions[2], map);
-                    } else if (decision.equals("R")){
-                        nextDecision((Integer) actions[3], map);
+                    System.out.println(actions[4]);
+                    if(actions[4] != null){
+                        if (decision.equals("Y")) {
+                            nextDecision((Integer) actions[1], map, (String) actions[4]);
+                        } else if (decision.equals("N")) {
+                            nextDecision((Integer) actions[2], map, (String) actions[4]);
+                        } else if (decision.equals("R")){
+                            nextDecision((Integer) actions[3], map, (String) actions[4]);
+                        }
+                    } else{
+                        if (decision.equals("Y")) {
+                            nextDecision((Integer) actions[1], map);
+                        } else if (decision.equals("N")) {
+                            nextDecision((Integer) actions[2], map);
+                        } else if (decision.equals("R")){
+                            nextDecision((Integer) actions[3], map);
+                        }   
                     }
-                    
                 }
         }
     }
-    
+       static void nextDecision(Integer nextPos, HashMap map, String endMessage){
+        while(true){
+                if(nextPos == 0){
+                   System.out.println("You lived life happily ever after. Bye bye.");
+                   System.exit(0);
+                } else if(nextPos == -1){
+                   System.out.println("You failed and died. The mission has failed. The end.");
+                   System.exit(0);
+                } else{
+                    Object[] actions = (Object[]) map.get(nextPos);
+                    System.out.println("");
+                    System.out.println(actions[0] + ": "); //The question
+                    System.out.print("[Your Input (Y/N/R)] ~ ");
+                    String decision = input.nextLine();
+                    if (decision.equals("Y")) {
+                        nextDecision((Integer) actions[1], map, (String) actions[4]);
+                    } else if (decision.equals("N")) {
+                        nextDecision((Integer) actions[2], map, (String) actions[4]);
+                    } else if (decision.equals("R")){
+                        nextDecision((Integer) actions[3], map, (String) actions[4]);
+                    }
+                }
+        }
+    }
     static void printBreak(){
         System.out.println("--------------------------------------");
     }
     
     public static void main(String[] args) {
+        // The biggest flaw in my program are these hash maps
+        
         HashMap<Integer, Object[]> teamMap = new HashMap<Integer, Object[]>(){{
-            put(1, new Object[]{"Do you fight?", 2, 3, 1});
-            put(2, new Object[]{"Will you leave at night", 4, 5, 1});
-            put(3, new Object[]{"Will you sleep?", 0, 0, 1});
-            put(4, new Object[]{"The war was over, before you arrived. Would you like to go back home?", 0, 0, 2});
+            put(1, new Object[]{"You are going to Mordor with the dwarves. Will you ride on horse", 2, 3, 1});
+            put(2, new Object[]{"You rode fast to Rivendell, where the elves coaxed you into leaking the dwarves' plan. Will you tell them?", 0, 4, 1, "Why'd you do that, the dwarves don't trust you, the elves didn't give you a bribe and you got sent back home. The mission failed"});
+            put(3, new Object[]{"You were walking along, enjoying your time when you suddenly encountered massive trolls who attempted to cook you. Do you escape?", -2, 0, 1});
+            
+            put(4, new Object[]{"You told the elves ", 0, 0, 2});
             put(5, new Object[]{"You went to sleep instead.", 0, 0, 2});
         }};
         
