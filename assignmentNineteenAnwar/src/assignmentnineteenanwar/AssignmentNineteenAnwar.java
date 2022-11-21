0a592a05232310fde76bd715a7c60ee9e9bec54a
Mohammad Anwar <mohammad.anwar2939@gmail.com>
2022-11-21 09:20:15 -0500

    Add files via upload

---
+++ assignmentNineteenAnwar/src/assignmentnineteenanwar/AssignmentNineteenAnwar.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignmentnineteenanwar;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author s201076699
+ */
+public class AssignmentNineteenAnwar {
+    // Declare and initalize Scanner for input
+    static Scanner keyedInput = new Scanner (System.in);
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
