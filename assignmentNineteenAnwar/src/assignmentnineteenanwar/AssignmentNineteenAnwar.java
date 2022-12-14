3c5f5c27d5a8c26747529ee291e5bc3588cc844c
mh-anwar <mohammad.anwar2939@gmail.com>
2022-12-14 10:40:15 -0500

    Almost done assign19

--- assignmentNineteenAnwar/src/assignmentnineteenanwar/AssignmentNineteenAnwar.java
+++ assignmentNineteenAnwar/src/assignmentnineteenanwar/AssignmentNineteenAnwar.java
@@ -23,10 +23,13 @@ public class AssignmentNineteenAnwar {
         while(userExit == false){
             System.out.println("-----------");
             System.out.println("Welcome to the Monkey Math Man");
+            // Output menu
             System.out.println("Enter the following numbers for different math options");
             System.out.println("1. Area of triangle \n2. Hypotenuse of a triangle \n3. Speed of a vehicle \n4. Midpoint of a line \n5. Missing angle of right triangle\n6. Exit");
+            // Get menu
             System.out.print(">>> ");
             int input = keyedInput.nextInt();
+            // Call sub-routine based on choice
             if(input==1){
                 triangleArea();
             } else if(input==2){
@@ -44,26 +47,35 @@ public class AssignmentNineteenAnwar {
     }
     
     static void triangleArea(){
+        // Calculate area of triangle
         // a = bh/2
         System.out.println("Welcome to Triangle Area Calculator");
+        // Get the base length of the triangle
         System.out.println("What is the base of the triangle?");
         int base = keyedInput.nextInt();
+        // Get the height length of the triangle
         System.out.println("What is the height of the triangle?");
         int height = keyedInput.nextInt();
+        // Calculate the area and output it
         int triangleArea = (base * height)/2;
         System.out.println("The area of the triangle is: " + triangleArea);
     }
     static void triangleHypotenuse(){
+        // Calculate the triangle hypotenuse
         // a^2 + b^2 = c^2
         System.out.println("Welcome to Triangle Hypotenuse Calculator");
+        // Get side a of the triangle
         System.out.println("What is side A of the triangle?");
         int sideA = keyedInput.nextInt();
+        // Get side b of the triangle
         System.out.println("What is side B of the triangle?");
         int sideB = keyedInput.nextInt();
-        Double triangleArea = Math.sqrt((sideA^2) + (sideB^2));
-        System.out.println("The hypotenuse of the triangle is: " + triangleArea);
+        // Calculate the triangle hypotenuse and output it
+        Double triangleHypotenuse = Math.sqrt((sideA^2) + (sideB^2));
+        System.out.println("The hypotenuse of the triangle is: " + triangleHypotenuse);
     }
     static void lineMidpoint(){
+        // Calculate the line midpoint
         // a^2 + b^2 = c^2
         System.out.println("Welcome to Line Segment Midpoint Calculator");
         System.out.println("What is the X co-ordinate of the first point?");
@@ -75,16 +87,28 @@ public class AssignmentNineteenAnwar {
         System.out.println("What is the Y co-ordinate of the second point?");
         int secondY = keyedInput.nextInt();
         
-        int xValues = (firstX + secondX)/2;
-        int yValues = (firstY + secondY)/2;
-        int midpoint = xValues + yValues;
-        System.out.println("The midpoint of the line segment is: " + midpoint);
+        int xValueMid = (firstX + secondX)/2;
+        int yValueMid = (firstY + secondY)/2;
+        System.out.println("The midpoint of the line segment is: " + xValueMid + " " + yValueMid);
        
     }
     static void vehicleSpeed(){
-        
+        System.out.println("Welcome to Vehicle Speed Calculator");
+        System.out.println("What was the time it took the car to travel?");
+        Double carTime = keyedInput.nextDouble();
+        System.out.println("What was the distance the car travelled?");
+        Double carDistance = keyedInput.nextDouble();
+        Double carSpeed = carDistance/carTime;
+        System.out.println("The car speed is: " + carSpeed);
     }
     static void triangleMissingAngle(){
-        
+        final int totalAngle = 180;
+        System.out.println("Welcome to Triangle Missing Angle Calculator");
+        System.out.println("What is angle A of the triangle?");
+        Double angleA = keyedInput.nextDouble();
+        System.out.println("What is angle B of the triangle?");
+        Double angleB = keyedInput.nextDouble();
+        Double missingAngle = totalAngle - angleA - angleB;
+        System.out.println("The missing angle is: " + missingAngle + " degrees");
     }
 }
