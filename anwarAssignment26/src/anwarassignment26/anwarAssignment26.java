193dd71f72bfa2bfa71304d30693d5f2f08b5a05
mh-anwar <mohammad.anwar2939@gmail.com>
2023-01-25 01:06:44 -0500

    Partially funcional A26

--- anwarAssignment26/src/anwarassignment26/anwarAssignment26.java
+++ anwarAssignment26/src/anwarassignment26/anwarAssignment26.java
@@ -31,17 +31,21 @@ public class anwarAssignment26 extends javax.swing.JFrame {
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         txtDecimal = new javax.swing.JTextField();
-        btnDecToBin = new javax.swing.JButton();
-        btnDecToBin1 = new javax.swing.JButton();
+        btnDecimalToBinary = new javax.swing.JButton();
+        btnBinaryToDecimal = new javax.swing.JButton();
         jLabel4 = new javax.swing.JLabel();
         txtBinary = new javax.swing.JTextField();
-        btnDecToBin2 = new javax.swing.JButton();
+        btnDecimalToHex = new javax.swing.JButton();
         jLabel5 = new javax.swing.JLabel();
-        jTextField3 = new javax.swing.JTextField();
+        txtDecimalToHex = new javax.swing.JTextField();
         jLabel6 = new javax.swing.JLabel();
-        btnHextToDec = new javax.swing.JButton();
+        btnHexToDecimal = new javax.swing.JButton();
         jLabel7 = new javax.swing.JLabel();
         txtHexadecimal = new javax.swing.JTextField();
+        lblBinaryToDecimal = new javax.swing.JLabel();
+        lblDecimalToBinary = new javax.swing.JLabel();
+        lblDecimalToHex = new javax.swing.JLabel();
+        lblHexToDecimal = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -51,26 +55,26 @@ public class anwarAssignment26 extends javax.swing.JFrame {
 
         jLabel3.setText("Enter a decimal number:");
 
-        btnDecToBin.setText("Dec. To Bin.");
-        btnDecToBin.addActionListener(new java.awt.event.ActionListener() {
+        btnDecimalToBinary.setText("Dec. To Bin.");
+        btnDecimalToBinary.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnDecToBinActionPerformed(evt);
+                btnDecimalToBinaryActionPerformed(evt);
             }
         });
 
-        btnDecToBin1.setText("Bin. To Dec.");
-        btnDecToBin1.addActionListener(new java.awt.event.ActionListener() {
+        btnBinaryToDecimal.setText("Bin. To Dec.");
+        btnBinaryToDecimal.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnDecToBin1ActionPerformed(evt);
+                btnBinaryToDecimalActionPerformed(evt);
             }
         });
 
         jLabel4.setText("Enter a binary number:");
 
-        btnDecToBin2.setText("Dec. To Hex.");
-        btnDecToBin2.addActionListener(new java.awt.event.ActionListener() {
+        btnDecimalToHex.setText("Dec. To Hex.");
+        btnDecimalToHex.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnDecToBin2ActionPerformed(evt);
+                btnDecimalToHexActionPerformed(evt);
             }
         });
 
@@ -78,10 +82,10 @@ public class anwarAssignment26 extends javax.swing.JFrame {
 
         jLabel6.setText("Decimal and Hexadecimal");
 
-        btnHextToDec.setText("Hex. to Dec.");
-        btnHextToDec.addActionListener(new java.awt.event.ActionListener() {
+        btnHexToDecimal.setText("Hex. to Dec.");
+        btnHexToDecimal.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnHextToDecActionPerformed(evt);
+                btnHexToDecimalActionPerformed(evt);
             }
         });
 
@@ -116,25 +120,34 @@ public class anwarAssignment26 extends javax.swing.JFrame {
                                         .addComponent(txtHexadecimal, javax.swing.GroupLayout.PREFERRED_SIZE, 86, javax.swing.GroupLayout.PREFERRED_SIZE)
                                         .addGap(51, 51, 51)
                                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                            .addComponent(btnDecToBin1)
-                                            .addComponent(btnDecToBin)
-                                            .addComponent(btnDecToBin2)
-                                            .addComponent(btnHextToDec)))
+                                            .addComponent(btnBinaryToDecimal)
+                                            .addComponent(btnDecimalToBinary)
+                                            .addComponent(btnDecimalToHex)
+                                            .addComponent(btnHexToDecimal)))
                                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                             .addComponent(txtBinary, javax.swing.GroupLayout.PREFERRED_SIZE, 86, javax.swing.GroupLayout.PREFERRED_SIZE)
                                             .addComponent(txtDecimal, javax.swing.GroupLayout.PREFERRED_SIZE, 86, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                        .addGap(202, 202, 202))))))
+                                        .addGap(202, 202, 202))))
+                            .addComponent(jLabel2)))
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, 86, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(txtDecimalToHex, javax.swing.GroupLayout.PREFERRED_SIZE, 86, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(jLabel6))
                         .addGap(202, 202, 202)))
-                .addContainerGap(51, Short.MAX_VALUE))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jLabel2)
-                .addGap(271, 271, 271))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(8, 8, 8)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(lblDecimalToBinary, javax.swing.GroupLayout.DEFAULT_SIZE, 344, Short.MAX_VALUE)
+                                .addContainerGap())
+                            .addComponent(lblBinaryToDecimal, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(lblDecimalToHex, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(lblHexToDecimal, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addContainerGap())))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -144,82 +157,143 @@ public class anwarAssignment26 extends javax.swing.JFrame {
                 .addGap(30, 30, 30)
                 .addComponent(jLabel2)
                 .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel3)
-                    .addComponent(txtDecimal, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(btnDecToBin))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(lblDecimalToBinary, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(jLabel3)
+                        .addComponent(txtDecimal, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(btnDecimalToBinary)))
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel4)
                     .addComponent(txtBinary, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(btnDecToBin1))
+                    .addComponent(btnBinaryToDecimal)
+                    .addComponent(lblBinaryToDecimal, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(35, 35, 35)
                 .addComponent(jLabel6)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel5)
-                    .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(btnDecToBin2))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(lblDecimalToHex, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(jLabel5)
+                        .addComponent(txtDecimalToHex, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(btnDecimalToHex)))
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel7)
                     .addComponent(txtHexadecimal, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(btnHextToDec))
-                .addContainerGap(10, Short.MAX_VALUE))
+                    .addComponent(btnHexToDecimal)
+                    .addComponent(lblHexToDecimal, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 22, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void btnDecToBinActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnDecToBinActionPerformed
-        // DATA VAL
-        int decimalNumber = Integer.parseInt(txtDecimal.getText());
+    private void btnDecimalToBinaryActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnDecimalToBinaryActionPerformed
+        // Declare the blank decimal number
+        int decimalNumber = 0;
+        
+        // Try to get the value of the decimal number from textfield
+        try{
+            decimalNumber = Integer.parseInt(txtDecimal.getText());
+        } catch(Exception e){
+            lblDecimalToBinary.setText("Please enter a number.");
+        }
+        
+        // Declare string and int to hold the binary num and remainder respectively
         String binaryNumber = "";
         int remainder;
+        
+        // Loop until the value of the decimal number is not less than 1 (
         while(decimalNumber != 1/2){
+            // Get the remainder of the decimal divided by 2
             remainder = decimalNumber % 2;
-            decimalNumber = decimalNumber / 2;
+            // Divide it by 2 for next iteration
+            decimalNumber /= 2;
+            // Add the remainder to the binary number (either 0 or 1)
             binaryNumber += Integer.toString(remainder);
         }
-        System.out.println(binaryNumber);
-    }//GEN-LAST:event_btnDecToBinActionPerformed
+        lblDecimalToBinary.setText(binaryNumber);
+    }//GEN-LAST:event_btnDecimalToBinaryActionPerformed
 
-    private void btnDecToBin1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnDecToBin1ActionPerformed
+    private void btnBinaryToDecimalActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnBinaryToDecimalActionPerformed
+        final int baseTwo = 2;
         String binaryNumber = txtBinary.getText();
         int decimalNumber = 0;
         double power = 0;
+        
+        // Reverse loop through the binary number
         for(int i = binaryNumber.length()-1; i >= 0; i--){
-            
+            // Get the number at position i
             int binaryNum = Character.getNumericValue(binaryNumber.charAt(i));
-            int baseTwoNum = binaryNum * (int) Math.pow(2, power);
+            // Multiply the number by 2 to a power to get the base two number
+            int baseTwoNum = binaryNum * (int) Math.pow(baseTwo, power);
+            // Add the base two number to the toal decimal value
             decimalNumber += baseTwoNum;
+            // Increase power by 1 for next loop
             power+=1;
         }
-        System.out.println(decimalNumber);
-    }//GEN-LAST:event_btnDecToBin1ActionPerformed
+        lblBinaryToDecimal.setText(Integer.toString(decimalNumber));
+    }//GEN-LAST:event_btnBinaryToDecimalActionPerformed
+        static final char[] HEX_LETTERS = {'A', 'B', 'C', 'D', 'E', 'F'};
+        static final char[] HEX_NUMBERS = {10, 11, 12, 13, 14, 15};
+    private void btnDecimalToHexActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnDecimalToHexActionPerformed
+        // Declare the blank decimal number
+        int decimalNumber = 0;
+        
+        // Try to get the value of the decimal number from textfield
+        try{
+            decimalNumber = Integer.parseInt(txtDecimalToHex.getText());
+        } catch(Exception e){
+            lblDecimalToHex.setText("Please enter a number.");
+        }
+        
+        // Declare string and int to hold the hexadecimal num and remainder respectively
+        String hexadecimal = "";
+        int remainder;
+        
+        // Loop until the value of the decimal number is not less than 1 (
+        while(decimalNumber != 1/2){
+            // Get the remainder of the decimal divided by 16 (because hexadecimal is base 16)
+            remainder = decimalNumber % 16;
+            // Divide it by 16 for next iteration
+            decimalNumber /= 16;
+            // Convert hexadecimal to letter, if it is letter
+            if(remainder >= 10){
+                int letterPosition = new String(HEX_NUMBERS).indexOf(remainder);
+                
+                hexadecimal += HEX_LETTERS[letterPosition];
+            } else{
+                
+                hexadecimal += Integer.toString(remainder);
+            }
+            System.out.println(remainder);
+            // Add the remainder to the binary number (either 0 or 1)
+        }
+        lblDecimalToHex.setText(hexadecimal);
+    }//GEN-LAST:event_btnDecimalToHexActionPerformed
 
-    private void btnDecToBin2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnDecToBin2ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_btnDecToBin2ActionPerformed
-
-    private void btnHextToDecActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnHextToDecActionPerformed
-        String hexNumber = txtHexadecimal.getText();
+    private void btnHexToDecimalActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnHexToDecimalActionPerformed
+        String hexString = txtHexadecimal.getText();
         int decimalNumber = 0;
         double power = 0;
-        char[] letterToHex = {'A'};
-        int[] hexNumbersList = {11};
-        for(int i = hexNumber.length()-1; i >= 0; i--){
-            char currentLetter = hexNumber.charAt(i);
-            if(new String(letterToHex).indexOf(currentLetter)){
-                
-            }
-            int binaryNum = Character.getNumericValue(binaryNumber.charAt(i));
-            int baseTwoNum = binaryNum * (int) Math.pow(2, power);
-            decimalNumber += baseTwoNum;
+        for(int i = hexString.length()-1; i >= 0; i--){
+            
+            int currentChar = Character.getNumericValue(hexString.charAt(i));
+            // Check if the current character is a letter
+            int letterPosition = new String(HEX_LETTERS).indexOf(currentChar);
+            
+            if(letterPosition >= 0){
+                currentChar =  (int)  HEX_NUMBERS[letterPosition];
+            } 
+            
+            int baseSixteenNum = currentChar * (int) Math.pow(16, power);
+            decimalNumber += baseSixteenNum;
             power+=1;
         }
-        System.out.println(decimalNumber);
-    }//GEN-LAST:event_btnHextToDecActionPerformed
+        lblHexToDecimal.setText(Integer.toString(decimalNumber));
+    }//GEN-LAST:event_btnHexToDecimalActionPerformed
 
     private void txtHexadecimalActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtHexadecimalActionPerformed
         // TODO add your handling code here:
@@ -261,10 +335,10 @@ public class anwarAssignment26 extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btnDecToBin;
-    private javax.swing.JButton btnDecToBin1;
-    private javax.swing.JButton btnDecToBin2;
-    private javax.swing.JButton btnHextToDec;
+    private javax.swing.JButton btnBinaryToDecimal;
+    private javax.swing.JButton btnDecimalToBinary;
+    private javax.swing.JButton btnDecimalToHex;
+    private javax.swing.JButton btnHexToDecimal;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
@@ -272,9 +346,13 @@ public class anwarAssignment26 extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel jLabel7;
-    private javax.swing.JTextField jTextField3;
+    private javax.swing.JLabel lblBinaryToDecimal;
+    private javax.swing.JLabel lblDecimalToBinary;
+    private javax.swing.JLabel lblDecimalToHex;
+    private javax.swing.JLabel lblHexToDecimal;
     private javax.swing.JTextField txtBinary;
     private javax.swing.JTextField txtDecimal;
+    private javax.swing.JTextField txtDecimalToHex;
     private javax.swing.JTextField txtHexadecimal;
     // End of variables declaration//GEN-END:variables
 }
